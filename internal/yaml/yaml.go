ce1f650f498d8b05bfd3cfd3ebc06dd1c98e820b
Yuki Ito <mrno110y@gmail.com>
2021-11-21 21:01:42 +0900

    internal/yaml: implement

---
+++ internal/yaml/yaml.go
@@ -0,0 +1,79 @@
+package yaml
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+
+	"github.com/110y/splanter/internal/model"
+)
+
+type Loader struct{}
+
+func NewLoader() *Loader {
+	return &Loader{}
+}
+
+func (l *Loader) Load(ctx context.Context, dir string) ([]*model.Table, error) {
+	var tables []*model.Table
+	err := filepath.WalkDir(dir, func(path string, entry fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		fname := entry.Name()
+		ext := filepath.Ext(fname)
+		if !entry.IsDir() && (ext == ".yaml" || ext == ".yml") {
+			var items []yaml.MapSlice
+			file, err := os.Open(path)
+			if err != nil {
+				return fmt.Errorf("failed to open yaml file: %w", err)
+			}
+
+			seeds, err := io.ReadAll(file)
+			if err != nil {
+				return fmt.Errorf("failed to read yaml file: %w", err)
+			}
+
+			if err = yaml.Unmarshal(seeds, &items); err != nil {
+				return fmt.Errorf("failed to unmarshal yaml file: %w", err)
+			}
+
+			records := make([]*model.Record, len(items))
+			for i, proparties := range items {
+				records[i] = &model.Record{
+					Values: make(map[string]interface{}),
+				}
+
+				for _, p := range proparties {
+					key, ok := p.Key.(string)
+					if !ok {
+						return fmt.Errorf("failed to unmarshal yaml proparty: %w", err)
+					}
+
+					records[i].Values[key] = p.Value
+				}
+
+			}
+
+			name := filepath.Base(strings.TrimSuffix(fname, ext))
+			tables = append(tables, &model.Table{
+				Name:    name,
+				Records: records,
+			})
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to walk dir %s: %w", dir, err)
+	}
+
+	return tables, nil
+}
---
+++ internal/yaml/yaml_test.go
@@ -0,0 +1,107 @@
+package yaml
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/110y/splanter/internal/model"
+)
+
+func TestLoad(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	loader := NewLoader()
+	actual, err := loader.Load(ctx, "testdata/seeds")
+	if err != nil {
+		t.Errorf("failed to load seeds: %s", err)
+		return
+	}
+
+	expected := []*model.Table{
+		{
+			Name: "Bar",
+			Records: []*model.Record{
+				{
+					Values: map[string]interface{}{
+						"FooID": "e70946a8-2fb8-4457-96b1-d64c0d8d124c",
+						"BarID": "208b6571-c140-4c2b-a9d5-b581fb062a77",
+						"Name":  "bar1",
+					},
+				},
+				{
+					Values: map[string]interface{}{
+						"FooID": "0b64da62-5895-4a7d-97bd-928ac8aaa076",
+						"BarID": "10bb9433-559a-4b03-9361-d9cce55ec17c",
+						"Name":  "bar2",
+					},
+				},
+				{
+					Values: map[string]interface{}{
+						"FooID": "e70946a8-2fb8-4457-96b1-d64c0d8d124c",
+						"BarID": "bcc0d0df-81bf-41b5-9427-9deb5e8f76f4",
+						"Name":  "bar3",
+					},
+				},
+			},
+		},
+		{
+			Name: "Baz",
+			Records: []*model.Record{
+				{
+					Values: map[string]interface{}{
+						"FooID": "e70946a8-2fb8-4457-96b1-d64c0d8d124c",
+						"BarID": "208b6571-c140-4c2b-a9d5-b581fb062a77",
+						"BazID": "748eb1a4-6c2b-44d2-a549-db725865d9d6",
+						"Name":  "baz1",
+					},
+				},
+				{
+					Values: map[string]interface{}{
+						"FooID": "0b64da62-5895-4a7d-97bd-928ac8aaa076",
+						"BarID": "10bb9433-559a-4b03-9361-d9cce55ec17c",
+						"BazID": "373388b5-a7e4-4112-a898-ac0818ceefa4",
+						"Name":  "baz2",
+					},
+				},
+				{
+					Values: map[string]interface{}{
+						"FooID": "e70946a8-2fb8-4457-96b1-d64c0d8d124c",
+						"BarID": "bcc0d0df-81bf-41b5-9427-9deb5e8f76f4",
+						"BazID": "8c8cafab-830c-4f12-a5d8-a6bda10e912f",
+						"Name":  "baz3",
+					},
+				},
+			},
+		},
+		{
+			Name: "Foo",
+			Records: []*model.Record{
+				{
+					Values: map[string]interface{}{
+						"FooID": "e70946a8-2fb8-4457-96b1-d64c0d8d124c",
+						"Name":  "foo1",
+					},
+				},
+				{
+					Values: map[string]interface{}{
+						"FooID": "0b64da62-5895-4a7d-97bd-928ac8aaa076",
+						"Name":  "foo2",
+					},
+				},
+				{
+					Values: map[string]interface{}{
+						"FooID": "bcc0d0df-81bf-41b5-9427-9deb5e8f76f4",
+						"Name":  "foo3",
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(actual, expected); diff != "" {
+		t.Errorf("\n(-actual, +expected)\n%s", diff)
+	}
+}
